501d94ec551110c937ad3c7da0b816645fa0be83
Torin <extrasmores@gmail.com>
2023-06-21 16:13:52 -0600

    fixing

--- index.js
+++ index.js
@@ -3,41 +3,36 @@ const moviesWrapper = document.querySelector('.movies');
 
 moviesWrapper.classList += ' movies__loading'
 
-// if (!movies) {
-//   movies = 
-
-// }
-// moviesWrapper.classList.remove('movies__loading')
-
 
 async function renderMovies(filter) {
-    event.preventDefault();
-    const searchButton = document.getElementById('searchButton');
-    const searchInput = document.querySelector('#searchInput');
-    const query = searchInput.value;
-    moviesWrapper.classList.add('movies__loading');
-    searchButton.innerHTML = '<i class="fas fa-spinner fa-spin"></i>';
-    const movies = await fetch(`http://www.omdbapi.com/?apikey=1f280777&s=${query}`)
-    const moviesData = await movies.json();
-    const moviesArray = moviesData.Search;
-
-    if (filter === "NEW_TO_OLD") {
-        moviesArray.sort((a, b) => (b.Year) - (a.Year));
-      } else if (filter === "OLD_TO_NEW") {
-         moviesArray.sort((a, b) => (a.Year) - (b.Year));
+        event.preventDefault();
+        // const searchButton = document.getElementById('searchButton');
+        const searchInput = document.querySelector('#searchInput');
+        const query = searchInput.value;
+        moviesWrapper.classList.add('movies__loading');
+        // searchButton.innerHTML = '<i class="fas fa-spinner fa-spin"></i>';
+        const movies = await fetch(`http://www.omdbapi.com/?apikey=1f280777&s=${query}`);
+        const moviesData = await movies.json();
+        const moviesArray = moviesData.Search;
+      
+        if (filter === "NEW_TO_OLD") {
+          moviesArray.sort((a, b) => b.Year - a.Year);
+        } else if (filter === "OLD_TO_NEW") {
+          moviesArray.sort((a, b) => a.Year - b.Year);
+        // } else if (filter === "ALPHABETICALLY") {
+        //   moviesArray.sort((a, b) => a.Title.localeCompare(b.Title));
+        // }
+      
+        moviesWrapper.innerHTML = moviesArray.map(movie => getMovieHTML(movie)).join("");
+        moviesWrapper.classList.remove('movies__loading');
+        // searchButton.innerHTML = '<i class="fa-solid fa-magnifying-glass"></i>';
       }
-    
+    }
 
-    moviesWrapper.innerHTML = moviesArray.map(movie => getMovieHTML(movie)).join("");
-    moviesWrapper.classList.remove('movies__loading');
-    searchButton.innerHTML = '<i class="fa-solid fa-magnifying-glass"></i>';
-}
 
 function filterMovies(event) {
     renderMovies(event.target.value);
   }
-  
-
   
 setTimeout(() => {
     renderMovies();
